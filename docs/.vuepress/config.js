4dfd2a2bf938216603cc0228e5017d86c32235ef
vandvassily <zhs25427199@gmail.com>
2020-03-14 13:12:03 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    base: '/fe-study/',
+    title: 'fe-study',
+    description: '前端知识学习',
+    ga: 'UA-117533223-1',
+    serviceWorker: true,
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-117533223-1'
+            }
+        ]
+    ],
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        nav: [
+            { text: 'Blog', link: 'http://vandvassily.cn/' },
+            { text: 'Github', link: 'https://github.com/vandvassily/fe-study' },
+        ],
+        sidebar: [
+            {
+                title: 'JS知识',
+                collapsable: false,
+                children: [
+                    ['/js_study/', '单行及多行省略号']
+                ]
+            },
+            {
+                title: 'CSS知识',
+                collapsable: false,
+                children: [
+                    ['/css_study/', 'rem初始化']
+                ]
+            }
+        ]
+    }
+}
