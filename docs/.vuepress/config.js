bc664052f0c596ae179f3b95c250e38881f86812
vandvassily <zhs25427199@gmail.com>
2020-03-17 18:05:04 +0800

    修改配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,10 @@
 module.exports = {
     base: '/fe-study/',
     title: 'fe-study',
-    description: '前端知识学习',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
+    description: '前端知识积累',
     ga: 'UA-117533223-1',
     serviceWorker: true,
     plugins: [
@@ -16,7 +19,11 @@ module.exports = {
         lineNumbers: true
     },
     themeConfig: {
+        logo: '/img/logo.jpg',
+        sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+        lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
         nav: [
+            { text: 'Home', link: '/' },
             { text: 'Blog', link: 'http://vandvassily.cn/' },
             { text: 'Github', link: 'https://github.com/vandvassily/fe-study' },
         ],
