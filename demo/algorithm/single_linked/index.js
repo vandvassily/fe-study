ad12dc7b16710e2f9203e1cf3abd789bc66451c3
zhs25427199 <zhs25427199@gmail.com>
2020-08-24 00:00:49 +0800

    修改节流/添加单链表

---
+++ demo/algorithm/single_linked/index.js
@@ -0,0 +1,167 @@
+/**
+ * @file: 单链表
+ * @author: ZHS 
+ * @Date: 2020-08-23 12:23:50 
+ * @Last Modified by: ZHS
+ * @Last Modified time: 2020-08-23 23:58:16
+ */
+
+/**
+ * 功能：
+ * 1. 根据value查找
+ * 2. 根据index查找
+ * 3. 链表尾部插入
+ * 4. 指定元素向后插入
+ * 5. 查找前一个
+ * 6. 根据值删除
+ * 7. 遍历显示所有节点
+ */
+
+
+class Node {
+    constructor(value) {
+        this.data = value
+        this.next = null
+    }
+}
+
+
+class SingleLinked {
+    constructor(){
+        this.head = new Node(null)
+    }
+
+    // 根据值查找
+    findByValue(value) {
+        let temp = this.head
+
+        while(temp.data !== value && temp.next){
+            temp = temp.next
+        }
+
+        return temp.data === value ? temp : null
+    }
+
+    // 通过索引查找
+    findByIndex(index){
+        let pos = 0;
+        let temp = this.head
+
+        if(index === 0) {
+            return temp
+        }
+
+        while(temp.next && pos !== index) {
+            temp = temp.next
+            pos++
+        }
+
+        return pos === index ? temp : null;
+    }
+
+    // 尾部插入
+    append(value){
+        let temp = this.head
+
+        if(!temp.data) {
+            this.head.data = value
+            return;
+        }
+
+        while(temp.next){
+            temp = temp.next
+        }
+
+        temp.next = new Node(value)
+    }
+
+    // 指定节点向后插入
+    insert(newValue, value) {
+        let resultNode = this.findByValue(value);
+        if(!resultNode) {
+            console.log('未找到节点');
+            return;
+        }
+
+        let newNode = new Node(newValue)
+        newNode.next = resultNode.next
+        resultNode.next = newNode
+    }
+
+    // 查找前一个
+    findPrev(value) {
+        let temp = this.head
+
+        if(temp.data === value) {
+            console.log('没有前一个节点')
+            return null;
+        }
+
+        while(temp.next && temp.next.data !== value){
+            temp = temp.next
+        }
+
+        return temp.next && temp.next.data === value ? temp : null
+    }
+
+    // 根据值删除
+    deleteByValue(value) {
+        let curNode = this.findByValue(value)
+        if(!curNode){
+            console.log('当前值不存在')
+            return
+        }
+        let prevNode = this.findPrev(value)
+
+        if(!prevNode) {
+            this.head = curNode.next ? curNode.next : new Node(null)
+        }
+        else {
+            prevNode.next = prevNode.next.next
+        }
+    }
+
+    // 展示所有数据
+    display(){
+        let temp = this.head
+
+        while(temp.next) {
+            console.log(temp.data)
+            temp = temp.next
+        }
+
+        console.log(temp.data)
+    }
+}
+
+
+let singleLinked = new SingleLinked();
+
+singleLinked.append(12);
+singleLinked.append(13);
+singleLinked.append(14);
+singleLinked.append(15);
+
+console.log(JSON.parse(JSON.stringify(singleLinked)))
+// console.log(singleLinked.findByIndex(0));
+// console.log(singleLinked.findByIndex(5));
+// console.log(singleLinked.findByValue(11));
+// console.log(singleLinked.findByValue(12));
+// console.log(singleLinked.findByValue(16));
+singleLinked.display()
+
+console.log(singleLinked.findPrev(14));
+console.log(singleLinked.findPrev(16));
+console.log(singleLinked.deleteByValue(12));
+console.log(singleLinked.deleteByValue(13));
+console.log(singleLinked.deleteByValue(14));
+console.log(singleLinked.deleteByValue(15));
+// console.log(singleLinked)
+singleLinked.display()
+
+// singleLinked.insert(16, 15)
+// console.log(singleLinked.findByValue(16));
+
+// singleLinked.insert(17, 12)
+// console.log(singleLinked.findByValue(17));
+
--- demo/js_demo/throttle/index.js
+++ demo/js_demo/throttle/index.js
@@ -2,39 +2,74 @@ var count = 1;
 var container = document.getElementById('container');
 
 function getUserAction(e) {
-    console.log(e)
+    console.log(e);
     container.innerHTML = count++;
-    return '这是测试'
-};
+    return '这是测试';
+}
 
-container.onmousemove = throttle(getUserAction, 200)
+container.onmousemove = throttleTogether(getUserAction, 500);
 
 // 方案一： 使用时间戳
 function throttle(func, wait) {
     // 起始时间戳
-    var startTime = new Date().getTime();
-    var context = this;
+    var previousTime = 0;
+    var context;
 
     return function() {
+        context = this;
         var currentTime = new Date().getTime();
-        if(currentTime - startTime > wait) {
-            func.apply(context, arguments)
-            startTime = currentTime
+        if (currentTime - previousTime > wait) {
+            func.apply(context, arguments);
+            previousTime = currentTime;
         }
-    }
+    };
 }
 
 // 方案二： 使用定时器
 function throttle2(func, wait) {
-    var timer
-    var context = this
+    var timer, context;
 
     return function() {
-        var args = arguments
-        if(timer) return;
+        var args = arguments;
+        context = this;
+        if (timer) return;
         timer = setTimeout(function() {
-            func.apply(context, args)
-            timer = null
-        }, wait)
-    }
+            func.apply(context, args);
+            timer = null;
+        }, wait);
+    };
+}
+
+// 加入
+function throttleTogether(func, wait) {
+    var timer;
+    var context;
+    var previousTime = 0;
+
+    return function() {
+        var args = arguments;
+        context = this;
+
+        var currentTime = new Date().getTime();
+        var remaining = wait - (currentTime - previousTime);
+
+        if (remaining <= 0) {
+            // 剩余时间小于等于0，需要清空定时器，同时立即执行
+            if (timer) {
+                clearTimeout(timer);
+                timer = null;
+            }
+            console.log('直接执行');
+            func.apply(context, args);
+            previousTime = currentTime;
+        } else if (!timer) {
+            // 剩余时间大于0 且 定时器不存在，则设置剩余时间之后，执行方法
+            timer = setTimeout(function() {
+                console.log('timer执行');
+                func.apply(context, args);
+                timer = null;
+                previousTime = new Date().getTime();
+            }, remaining);
+        }
+    };
 }
