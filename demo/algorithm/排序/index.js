284105444157b014dbf56c38daecf8aa31e0784d
vandvassily <zhs25427199@gmail.com>
2020-12-24 21:01:48 +0800

    快排

--- demo/algorithm/排序/index.js
+++ demo/algorithm/排序/index.js
@@ -3,7 +3,7 @@ let arr = [1, 4, 2, 10, 6, 9, 22, 11, 32, -1, 12, 12];
 // for (let i = 0; i < 100000; i++) {
 //     arr2.push(Math.floor(Math.random() * 1000))
 // }
-console.log(arr)
+console.log(arr);
 /**
  * 冒泡排序
  * @param {Array} array 需排序数组
@@ -21,7 +21,7 @@ function buddleSort(array) {
         }
     }
 
-    console.log(`耗时${new Date().getTime() - start} ms`)
+    console.log(`耗时${new Date().getTime() - start} ms`);
 
     return array;
 }
@@ -39,16 +39,15 @@ function insertSort(array) {
         let value = array[i];
         let j = i - 1;
         for (; j >= 0; j--) {
-            if(array[j] > value) {
-                array[j + 1] = array[j]
-            }
-            else {
-                break
+            if (array[j] > value) {
+                array[j + 1] = array[j];
+            } else {
+                break;
             }
         }
-        array[j + 1] = value
+        array[j + 1] = value;
     }
-    console.log(`耗时${new Date().getTime() - start} ms`)
+    console.log(`耗时${new Date().getTime() - start} ms`);
     return array;
 }
 
@@ -63,16 +62,15 @@ function selectionSort(array) {
         let minIndex = i;
         let j = i;
         for (; j < array.length; j++) {
-            if(array[minIndex] > array[j]) {
-                minIndex = j
+            if (array[minIndex] > array[j]) {
+                minIndex = j;
             }
         }
         let temp = array[minIndex];
-        array[minIndex] = array[i]
+        array[minIndex] = array[i];
         array[i] = temp;
-        
     }
-    return array
+    return array;
 }
 
 // console.log(selectionSort(arr));
@@ -80,7 +78,7 @@ function selectionSort(array) {
 // 归并排序
 
 function mergeSort(arr) {
-    if(arr.length < 2) return arr;
+    if (arr.length < 2) return arr;
     let center = Math.floor(arr.length / 2);
     let left = mergeSort(arr.slice(0, center));
     let right = mergeSort(arr.slice(center));
@@ -88,34 +86,56 @@ function mergeSort(arr) {
 }
 
 /**
- * 
+ *
  * @param {Array} left 左数组
  * @param {Array} right 右数组
  */
 function merge(left, right) {
     let res = [];
-    let i = 0, j = 0;
+    let i = 0,
+        j = 0;
 
     while (i < left.length && j < right.length) {
         if (left[i] <= right[j]) {
             res.push(left[i]);
             i++;
-        }
-        else {
+        } else {
             res.push(right[i]);
             j++;
         }
     }
 
-    if(i < left.length) {
-        res.push(...left.slice(i))
+    if (i < left.length) {
+        res.push(...left.slice(i));
     }
 
-    if(j < right.length) {
+    if (j < right.length) {
         res.push(...right.slice(j));
     }
 
-    return res
+    return res;
 }
 
-console.log(mergeSort(arr));
+// console.log(mergeSort(arr));
+
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let center = Math.floor(arr.length / 2);
+    let left = [];
+    let right = [];
+    let middle = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < arr[center]) {
+            left.push(arr[i]);
+        } else if (arr[i] > arr[center]) {
+            right.push(arr[i]);
+        } else {
+            middle.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat(middle, quickSort(right));
+}
+
+console.log(quickSort(arr));
