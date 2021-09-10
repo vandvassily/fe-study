0a02c468e337ea36a65386b32992fcd38f1b2498
vandvassily <zhs25427199@gmail.com>
2021-09-10 15:25:24 +0800

    docs: 新增 lerna 文章

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,124 +1,120 @@
-const { path } = require('@vuepress/utils')
+const { path } = require('@vuepress/utils');
 
 module.exports = {
-    base: '/fe-study/',
-    title: 'fe-study',
-    head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }]
+  base: '/fe-study/',
+  title: 'fe-study',
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  description: '前端知识积累',
+  ga: 'UA-117533223-1',
+  serviceWorker: true,
+  plugins: [
+    [
+      '@vuepress/plugin-google-analytics',
+      {
+        ga: 'UA-117533223-1'
+      }
     ],
-    description: '前端知识积累',
-    ga: 'UA-117533223-1',
-    serviceWorker: true,
-    plugins: [
-        [
-            '@vuepress/plugin-google-analytics',
-            {
-                'ga': 'UA-117533223-1'
-            }
-        ],
-        [
-            '@vuepress/register-components',
-            {
-                componentsDir: path.resolve(__dirname, './components'),
-            },
-        ],
+    [
+      '@vuepress/register-components',
+      {
+        componentsDir: path.resolve(__dirname, './components')
+      }
+    ]
+  ],
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    logo: '/img/logo.jpg',
+    sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+    lastUpdated: true, // 文档更新时间：每个文件git最后提交的时间
+    repo: 'vandvassily/fe-study',
+    editLink: false,
+    navbar: [
+      { text: 'Home', link: '/' },
+      { text: 'Blog', link: 'http://vandvassily.cn/' }
     ],
-    markdown: {
-        lineNumbers: true
-    },
-    themeConfig: {
-        logo: '/img/logo.jpg',
-        sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-        lastUpdated: true, // 文档更新时间：每个文件git最后提交的时间
-        repo: 'vandvassily/fe-study',
-        editLink: false,
-        navbar: [
-            { text: 'Home', link: '/' },
-            { text: 'Blog', link: 'http://vandvassily.cn/' },
-        ],
-        sidebar: [
-            {
-                text: 'JS知识',
-                collapsable: false,
-                link: '/js_study/',
-                children: [
-                    '/js_study/basic/数据类型.md',
-                    '/js_study/basic/原型.md',
-                    '/js_study/basic/执行上下文.md',
-                    '/js_study/basic/this.md',
-                    '/js_study/basic/new.md',
-                    '/js_study/basic/call apply bind.md',
-                    '/js_study/basic/EventLoop.md',
-                    '/js_study/basic/array.md',
-                ]
-            },
-            {
-                text: 'CSS知识',
-                collapsable: false,
-                children: [
-                    '/css_study/'
-                ]
-            },
-            {
-                text: '数据结构',
-                collapsable: true,
-                link: '/structure/',
-                children: [
-                    '/structure/复杂度分析-上.md',
-                    '/structure/复杂度分析-下.md',
-                    '/structure/链表.md',
-                    '/structure/数组.md',
-                ]
-            },
-            {
-                text: '算法',
-                collapsable: true,
-                link: '/algorithm',
-                children: [
-                    '/algorithm/回溯算法.md',
-                    '/algorithm/链表.md',
-                    '/algorithm/排序.md',
-                    '/algorithm/字符串匹配.md'
-                ]
-            },
-            {
-                text: 'leetcode',
-                collapsable: true,
-                link: '/leetcode/',
-                children: [
-                    '/leetcode/19.删除链表倒数第N个结点.md'
-                ]
-            },
-            {
-                text: '正则',
-                collapsable: true,
-                link: '/regex/',
-                children: [
-                ]
-            },
-            {
-                text: 'Vue知识点',
-                collapsable: true,
-                link: '/vue/',
-                children: [
-                    '/vue/lifeCircle.md'
-                ]
-            },
-            {
-                text: '其他',
-                collapsable: false,
-                link: '/other/',
-                children: [
-                    '/other/indexedDB.md',
-                    '/other/npm link.md',
-                    '/other/vuePress.md',
-                    '/other/输入URL.md',
-                    '/other/babel.md',
-                    '/other/jenkins.md',
-                    '/other/nginx.md',
-                    '/other/rollup.md'
-                ]
-            }
+    sidebar: [
+      {
+        text: 'JS知识',
+        collapsable: false,
+        link: '/js_study/',
+        children: [
+          '/js_study/basic/数据类型.md',
+          '/js_study/basic/原型.md',
+          '/js_study/basic/词法作用域.md',
+          '/js_study/basic/执行上下文.md',
+          '/js_study/basic/执行上下文栈.md',
+          '/js_study/basic/作用域链.md',
+          '/js_study/basic/this.md',
+          '/js_study/basic/new.md',
+          '/js_study/basic/call apply bind.md',
+          '/js_study/basic/EventLoop.md',
+          '/js_study/basic/array.md',
+          '/js_study/basic/currying.md',
+          '/js_study/basic/深拷贝.md',
+          '/js_study/basic/debounce.md',
+          '/js_study/basic/throttle.md'
         ]
-    }
-}
+      },
+      {
+        text: 'CSS知识',
+        collapsable: false,
+        children: ['/css_study/']
+      },
+      {
+        text: '数据结构',
+        collapsable: true,
+        link: '/structure/',
+        children: [
+          '/structure/复杂度分析-上.md',
+          '/structure/复杂度分析-下.md',
+          '/structure/链表.md',
+          '/structure/数组.md'
+        ]
+      },
+      {
+        text: '算法',
+        collapsable: true,
+        link: '/algorithm',
+        children: ['/algorithm/回溯算法.md', '/algorithm/链表.md', '/algorithm/排序.md', '/algorithm/字符串匹配.md']
+      },
+      {
+        text: 'leetcode',
+        collapsable: true,
+        link: '/leetcode/',
+        children: ['/leetcode/19.删除链表倒数第N个结点.md']
+      },
+      {
+        text: '正则',
+        collapsable: true,
+        link: '/regex/',
+        children: []
+      },
+      {
+        text: 'Vue知识点',
+        collapsable: true,
+        link: '/vue/',
+        children: ['/vue/lifeCircle.md']
+      },
+      {
+        text: '其他',
+        collapsable: false,
+        link: '/other/',
+        children: [
+          '/other/输入URL.md',
+          '/other/indexedDB.md',
+          '/other/npm link.md',
+          '/other/vuePress.md',
+          '/other/babel.md',
+          '/other/jenkins.md',
+          '/other/nginx.md',
+          '/other/rollup.md',
+          '/other/lerna.md',
+          '/other/vue2改造为vite构建.md',
+          '/other/sentry.md'
+        ]
+      }
+    ]
+  }
+};
---
+++ docs/other/lerna.md
@@ -0,0 +1,106 @@
+# 基于 lerna 管理 monorepo 项目
+
+demo传送门: [lerna-example](https://github.com/vandvassily/lerna-example)
+
+## monorepo 与 multirepo
+
+Monorepo 的全称是 monolithic repository，即单体式仓库，与之对应的是 Multirepo(multiple repository)，这里的“单”和“多”是指每个仓库中所管理的模块数量。
+
+Monorepo 是把所有相关的 package 都放在一个仓库里进行管理，每个 package 独立发布。例如：React, Angular, Babel, Jest, Umijs, Vue ...
+
+Multirepo 是比较传统的做法，即每一个 package 都单独用一个仓库来进行管理。例如：Rollup, ...
+
+## Lerna
+
+Lerna 是一个管理多个 npm 模块的工具，是 Babel 自己用来维护自己的 Monorepo 并开源出的一个项目。优化维护多包的工作流，解决多个包互相依赖，且发布需要手动维护多个包的问题。
+
+Lerna 使用 git 和 npm 管理多软件包代码仓库的工作流程进行优化。
+
+### 安装
+
+```bash
+$ npm i -g lerna
+```
+
+### 初始化lerna项目
+
+```bash
+# --independent 独立版本号
+$ lerna init --independent
+```
+
+### 更改为 yarn worksapces
+
+目的是将所有子包的 `node_modules` 统一提升到 lerna 项目根目录下
+
+```json
+// ./lerna.json
+{
+  ...
+  "npmClient": "yarn",
+  "useWorkspaces": true
+}
+```
+
+```json
+// ./package.json
+{
+  "private": true,
+  ...
+  "workspaces": ["packages/*"]
+}
+```
+
+### 创建package
+
+```bash
+$ lerna create package-A
+$ lerna create package-B
+```
+
+### 添加依赖
+
+```bash
+# package-B 依赖于 package-A
+$ lerna add package-A --scope=package-B
+```
+
+### 安装包
+
+```bash
+# 根目录下
+$ yarn install
+```
+
+### 启动项目
+
+```bash
+# 命令可以写在根目录下 package.json 中的 scripts 中
+$ yarn workspace module-A dev
+```
+
+### 智能提示
+
+方便编辑器自动查找引用的依赖
+
+```json
+// jsconfig.json
+{ 
+  "compilerOptions": {
+    ...
+    "paths": {
+      "@vassily/h5-utils": ["./packages/h5-utils"],
+    }
+  }
+}
+```
+
+## 参考
+
+[现代前端工程化-基于 Monorepo 的 lerna](https://blog.csdn.net/qiwoo_weekly/article/details/115364654) 
+
+[每个前端都值得拥有自己的组件库，就像每个夏天都拥有西瓜](https://blog.csdn.net/xgangzai/article/details/119524117)
+
+[基于 lerna 的多包 JavaScript 项目搭建维护笔记](https://youngjuning.js.org/#/blog/others/%E5%9F%BA%E4%BA%8E%20lerna%20%E7%9A%84%E5%A4%9A%E5%8C%85%20java-script%20%E9%A1%B9%E7%9B%AE%E6%90%AD%E5%BB%BA)
+
+[基于 Lerna 管理 packages 的 Monorepo 项目最佳实践](https://juejin.cn/post/6844903911095025678)
