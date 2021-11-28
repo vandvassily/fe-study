953e94fa47799f556e87c95f9c02da22c18905b5
vandvassily <zhs25427199@gmail.com>
2021-11-28 22:35:32 +0800

    feat: 合并防抖和节流

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -53,8 +53,7 @@ module.exports = {
           '/js_study/basic/array.md',
           '/js_study/basic/currying.md',
           '/js_study/basic/深拷贝.md',
-          '/js_study/usage/debounce.md',
-          '/js_study/usage/throttle.md'
+          '/js_study/usage/debounce-throttle.md',
         ]
       },
       {
