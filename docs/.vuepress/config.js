2518e14af3385fe9bfd3f40573a8cbcca6f84d34
vandvassily <zhs25427199@gmail.com>
2021-12-02 23:26:50 +0800

    feat(all): 目录迁移
    目录迁移

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -38,65 +38,30 @@ module.exports = {
       {
         text: 'JS知识',
         collapsable: false,
-        link: '/js_study/',
+        link: '/javascript/',
         children: [
-          '/js_study/basic/数据类型.md',
-          '/js_study/basic/原型.md',
-          '/js_study/basic/词法作用域.md',
-          '/js_study/basic/执行上下文.md',
-          '/js_study/basic/执行上下文栈.md',
-          '/js_study/basic/作用域链.md',
-          '/js_study/basic/this.md',
-          '/js_study/basic/new.md',
-          '/js_study/basic/call apply bind.md',
-          '/js_study/basic/EventLoop.md',
-          '/js_study/basic/array.md',
-          '/js_study/basic/currying.md',
-          '/js_study/basic/深拷贝.md',
-          '/js_study/usage/debounce-throttle.md',
-        ]
-      },
-      // {
-      //   text: 'CSS知识',
-      //   collapsable: false,
-      //   children: ['/css_study/']
-      // },
-      {
-        text: '数据结构',
-        collapsable: true,
-        link: '/structure/',
-        children: [
-          '/structure/复杂度分析-上.md',
-          '/structure/复杂度分析-下.md',
-          '/structure/链表.md',
-          '/structure/数组.md'
+          '/javascript/basic/数据类型.md',
+          '/javascript/basic/原型.md',
+          '/javascript/basic/词法作用域.md',
+          '/javascript/basic/执行上下文.md',
+          '/javascript/basic/执行上下文栈.md',
+          '/javascript/basic/作用域链.md',
+          '/javascript/basic/this.md',
+          '/javascript/basic/new.md',
+          '/javascript/basic/call apply bind.md',
+          '/javascript/basic/EventLoop.md',
+          '/javascript/basic/array.md',
+          '/javascript/basic/currying.md',
+          '/javascript/basic/深拷贝.md',
+          '/javascript/usage/debounce-throttle.md',
         ]
       },
       {
-        text: '算法',
+        text: '浏览器',
         collapsable: false,
-        link: '/algorithm',
-        children: [
-          '/algorithm/链表.md',
-          '/algorithm/排序.md',
-          '/algorithm/二叉树的遍历.md',
-          '/algorithm/回溯算法.md',
-          '/algorithm/字符串匹配.md',
-          '/algorithm/dynamic-programming.md',
-        ]
+        link: '/browser/',
+        children: ['/browser/输入URL.md']
       },
-      // {
-      //   text: 'leetcode',
-      //   collapsable: false,
-      //   link: '/leetcode/',
-      //   children: ['/leetcode/19.删除链表倒数第N个结点.md']
-      // },
-      // {
-      //   text: '正则',
-      //   collapsable: true,
-      //   link: '/regex/',
-      //   children: []
-      // },
       {
         text: 'Vue',
         collapsable: true,
@@ -138,26 +103,76 @@ module.exports = {
         ]
       },
       {
+        text: 'NodeJs',
+        collapsable: true,
+        link: '/nodejs/',
+        children: [
+          '/nodejs/tools.md'
+        ]
+      },
+      {
+        text: '数据结构与算法',
+        collapsable: false,
+        link: '/structure-and-algorithm/',
+        children: [
+          '/structure-and-algorithm/复杂度分析-上.md',
+          '/structure-and-algorithm/复杂度分析-下.md',
+          '/structure-and-algorithm/数组.md',
+          '/structure-and-algorithm/链表.md',
+          '/structure-and-algorithm/排序.md',
+          '/structure-and-algorithm/二叉树的遍历.md',
+          '/structure-and-algorithm/回溯算法.md',
+          '/structure-and-algorithm/字符串匹配.md',
+          '/structure-and-algorithm/dynamic-programming.md',
+        ]
+      },
+      // {
+      //   text: 'leetcode',
+      //   collapsable: false,
+      //   link: '/leetcode/',
+      //   children: ['/leetcode/19.删除链表倒数第N个结点.md']
+      // },
+      {
+        text: '性能优化',
+        collapsable: true,
+        link: '/performance/',
+        children: [
+        ]
+      },
+      {
+        text: '前端工程化',
+        collapsable: false,
+        link: '/front-end-engineering/what-is-Frond-End-Engineering',
+        children: [
+          '/front-end-engineering/what-is-Frond-End-Engineering.md',
+          '/front-end-engineering/npm link.md',
+          '/front-end-engineering/babel.md',
+          '/front-end-engineering/jenkins.md',
+          '/front-end-engineering/rollup.md',
+          '/front-end-engineering/lerna.md',
+          '/front-end-engineering/vue2改造为vite构建.md',
+          '/front-end-engineering/vconsole.md',
+        ]
+      },
+      {
+        text: '程序设计和场景分析',
+        collapsable: false,
+        link: '/programming-design/',
+        children: [
+          '/programming-design/how-to-make-a-game.md',
+          '/programming-design/how-to-design-a-component-lib.md',
+          '/programming-design/how-to-design-a-web-monitor-system.md',
+        ]
+      },
+      {
         text: '其他',
         collapsable: false,
         link: '/other/',
         children: [
-          '/other/输入URL.md',
           '/other/indexedDB.md',
-          '/other/npm link.md',
           '/other/vuePress.md',
-          '/other/babel.md',
-          '/other/jenkins.md',
           '/other/nginx.md',
-          '/other/rollup.md',
-          '/other/lerna.md',
-          '/other/vue2改造为vite构建.md',
           '/other/sentry.md',
-          '/other/vconsole.md',
-          '/other/how-to-make-a-game.md',
-          '/other/how-to-design-a-component-lib.md',
-          '/other/how-to-design-a-web-monitor-system.md',
-          '/other/what-is-Frond-End-Engineering.md',
         ]
       },
       {
